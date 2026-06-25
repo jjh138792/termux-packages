1ed0defeb37a36c8c110f57c800cf42efb251db4
jjh138792 <jjh138792@outlook.com>
2026-06-26 07:15:57 +0800

    Create build.sh

---
+++ x11-packages/gfxstream/build.sh
@@ -0,0 +1,21 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/google/gfxstream
+TERMUX_PKG_DESCRIPTION="Graphics Streaming Kit (Gfxstream Backend)"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_VERSION=0.1.2
+TERMUX_PKG_DEPENDS="libc++, libdrm"
+
+termux_step_make() {
+    termux_setup_meson
+    
+    echo "[*] 直连 Google 官方独立仓库拉取 Gfxstream..."
+    git clone https://github.com/google/gfxstream.git $TERMUX_PKG_SRCDIR/gfxstream_src
+    cd $TERMUX_PKG_SRCDIR/gfxstream_src
+    
+    meson setup build \
+        --cross-file $TERMUX_MESON_CROSSFILE \
+        --prefix $TERMUX_PREFIX \
+        --libdir lib \
+        -Ddefault_library=shared
+        
+    ninja -C build install
+}
