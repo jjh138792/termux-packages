10a3540cb1622635990d96e21fd863899c3c4b96
jjh138792 <jjh138792@outlook.com>
2026-06-26 07:16:51 +0800

    Create build.sh

---
+++ x11-packages/rutabaga-gfx-ffi/build.sh
@@ -0,0 +1,36 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/magma-gpu/rutabaga_gfx
+TERMUX_PKG_DESCRIPTION="Rutabaga GFX FFI (Trinity Edition)"
+TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_VERSION=0.1.76
+TERMUX_PKG_SKIP_SRC_EXTRACT=true
+
+TERMUX_PKG_DEPENDS="gfxstream, virglrenderer, wayland"
+TERMUX_PKG_BUILD_DEPENDS="gfxstream, virglrenderer, wayland"
+
+termux_step_make() {
+    termux_setup_rust
+    rustup target add aarch64-linux-android
+    
+    echo "[*] 从 Github 官方源拉取最新版 Rutabaga GFX (带完整子模块)..."
+    git clone --recurse-submodules https://github.com/magma-gpu/rutabaga_gfx.git $TERMUX_PKG_SRCDIR/repo
+    
+    cd $TERMUX_PKG_SRCDIR/repo/ffi
+    
+    echo "[*] 启动 Cargo 编译，三神装全开！"
+    cargo build --release --target aarch64-linux-android --features="virgl_renderer,gfxstream,cross-domain"
+}
+
+termux_step_make_install() {
+    cd $TERMUX_PKG_SRCDIR/repo/ffi
+    install -Dm755 target/aarch64-linux-android/release/librutabaga_gfx_ffi.so $TERMUX_PREFIX/lib/librutabaga_gfx_ffi.so
+    install -Dm644 src/include/rutabaga_gfx_ffi.h $TERMUX_PREFIX/include/rutabaga_gfx/rutabaga_gfx_ffi.h
+    
+    mkdir -p $TERMUX_PREFIX/lib/pkgconfig
+    cat << PC_EOF > $TERMUX_PREFIX/lib/pkgconfig/rutabaga_gfx_ffi.pc
+Name: rutabaga_gfx_ffi
+Description: Rutabaga GFX FFI
+Version: 0.1.76
+Libs: -L${TERMUX_PREFIX}/lib -lrutabaga_gfx_ffi
+Cflags: -I${TERMUX_PREFIX}/include
+PC_EOF
+}
