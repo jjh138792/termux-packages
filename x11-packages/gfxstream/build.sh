8b6c775e178f4b408b8ebd6702d04f5f4c5cd4b7
jjh138792 <jjh138792@outlook.com>
2026-06-26 13:18:45 +0800

    Update build.sh

--- x11-packages/gfxstream/build.sh
+++ x11-packages/gfxstream/build.sh
@@ -2,10 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://github.com/google/gfxstream
 TERMUX_PKG_DESCRIPTION="Graphics Streaming Kit (Gfxstream Backend)"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_VERSION=1.0.0
-# ！！！神级魔法：直接用 git+ 协议，Termux 会自动拉取并处理子模块！
 TERMUX_PKG_SRCURL="git+https://github.com/google/gfxstream.git"
 TERMUX_PKG_GIT_BRANCH="main"
 TERMUX_PKG_DEPENDS="libc++, libdrm"
 
-# 告诉系统以共享动态库的方式编译，其他的全交给 Termux 自动处理！
+# ！！！【核心调教 Termux】！！！
+# 强制废弃水土不服的 CMake，启用 Termux 云端容器深度定制的 Meson 交叉构建器！
+TERMUX_PKG_BUILDER="meson"
+
+# 传递给 Meson 的标准参数
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Ddefault_library=shared"
