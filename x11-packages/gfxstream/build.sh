97db323f38fcc7f2e358fb1ce4159943c62ce39a
jjh138792 <jjh138792@outlook.com>
2026-06-26 12:57:13 +0800

    Update build.sh

--- x11-packages/gfxstream/build.sh
+++ x11-packages/gfxstream/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Graphics Streaming Kit (Gfxstream Backend)"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_VERSION=0.1.2
 TERMUX_PKG_DEPENDS="libc++, libdrm"
+TERMUX_PKG_BUILD_DEPENDS="meson, ninja"
 
 termux_step_make() {
     termux_setup_meson
