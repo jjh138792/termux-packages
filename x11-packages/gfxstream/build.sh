92d20880ec84c515f4fb51ac8e32774fd1d589ca
jjh138792 <jjh138792@outlook.com>
2026-06-26 18:37:52 +0800

    Update build.sh

--- x11-packages/gfxstream/build.sh
+++ x11-packages/gfxstream/build.sh
@@ -7,14 +7,14 @@ TERMUX_PKG_GIT_BRANCH="main"
 TERMUX_PKG_DEPENDS="libc++, libdrm, libx11"
 TERMUX_PKG_BUILD_DEPENDS="libx11"
 
-# 保持默认，让内部组件安分地编译成静态库
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=""
 
-# ！！！【真正的云端重塑：执行物理级强转与全局宏注入】！！！
+# ！！！【最后的手术：扫清 Vulkan 的 XCB 窗口撕裂】！！！
 termux_step_post_get_source() {
     echo "[*] 启动云端源码重塑，修复 Android NDK 与 X11 的跨界类型冲突..."
     local F1="host/gl/glestranslator/egl/egl_os_api_egl.cpp"
     local F2="host/gl/glestranslator/egl/egl_os_api_glx.cpp"
+    local F3="host/vulkan/display_surface_vk.cpp"
     
     # 精准强转 XGetGeometry 的 Window 参数
     sed -i 's/mGlxDisplay, win, /mGlxDisplay, (Drawable)(uintptr_t)win, /g' $F1
@@ -24,7 +24,9 @@ termux_step_post_get_source() {
     sed -i 's/GlxSurface::drawableFor(win)/(EGLNativeWindowType)(uintptr_t)GlxSurface::drawableFor(win)/g' $F2
     sed -i 's/new GlxSurface(wnd/new GlxSurface((GLXDrawable)(uintptr_t)wnd/g' $F2
     
+    # ！！！新加的致命一击：强转 Vulkan XCB 的窗口参数 ！！！
+    sed -i 's/\.window = window,/.window = (xcb_window_t)(uintptr_t)window,/g' $F3
+    
     echo "[*] 强行烙印安卓 Vulkan 上帝宏，彻底解锁跨平台结构体屏蔽！"
-    # 在 CMake 的第一行强行注入编译期宏定义！
     sed -i '1i add_compile_definitions(VK_USE_PLATFORM_ANDROID_KHR=1)' CMakeLists.txt
 }
