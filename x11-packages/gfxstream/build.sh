df2f5f934c79168a327a6a5083a5e353bc86d1f2
jjh138792 <jjh138792@outlook.com>
2026-06-26 13:06:10 +0800

    Update build.sh

--- x11-packages/gfxstream/build.sh
+++ x11-packages/gfxstream/build.sh
@@ -1,22 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/google/gfxstream
 TERMUX_PKG_DESCRIPTION="Graphics Streaming Kit (Gfxstream Backend)"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=0.1.2
+TERMUX_PKG_VERSION=1.0.0
+# ！！！神级魔法：直接用 git+ 协议，Termux 会自动拉取并处理子模块！
+TERMUX_PKG_SRCURL="git+https://github.com/google/gfxstream.git"
+TERMUX_PKG_GIT_BRANCH="main"
 TERMUX_PKG_DEPENDS="libc++, libdrm"
-TERMUX_PKG_BUILD_DEPENDS="meson, ninja"
 
-termux_step_make() {
-    termux_setup_meson
-    
-    echo "[*] 直连 Google 官方独立仓库拉取 Gfxstream..."
-    git clone https://github.com/google/gfxstream.git $TERMUX_PKG_SRCDIR/gfxstream_src
-    cd $TERMUX_PKG_SRCDIR/gfxstream_src
-    
-    meson setup build \
-        --cross-file $TERMUX_MESON_CROSSFILE \
-        --prefix $TERMUX_PREFIX \
-        --libdir lib \
-        -Ddefault_library=shared
-        
-    ninja -C build install
-}
+# 告诉系统以共享动态库的方式编译，其他的全交给 Termux 自动处理！
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Ddefault_library=shared"
