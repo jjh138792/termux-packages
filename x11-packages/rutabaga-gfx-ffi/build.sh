34c614a3866860254a3351f70f7bbd490b3cc1af
jjh138792 <jjh138792@outlook.com>
2026-06-27 08:33:22 +0800

    Update build.sh

--- x11-packages/rutabaga-gfx-ffi/build.sh
+++ x11-packages/rutabaga-gfx-ffi/build.sh
@@ -15,11 +15,24 @@ termux_step_make() {
         git clone --recurse-submodules https://github.com/magma-gpu/rutabaga_gfx.git $TERMUX_PKG_SRCDIR/repo
     fi
     
-    # ！！！【核心斩首行动：干掉上游的控制权，强制使用 Termux 工具链】！！！
     echo "[*] 物理粉碎上游自带的 rust-toolchain 劫持文件..."
     rm -f $TERMUX_PKG_SRCDIR/repo/rust-toolchain
     rm -f $TERMUX_PKG_SRCDIR/repo/rust-toolchain.toml
     
+    # ！！！【神级欺骗术：批量伪造子模块户口本，满足 Cargo 的强迫症】！！！
+    echo "[*] 批量下发 aemu_base 等子模块的虚假 pkg-config 证明..."
+    mkdir -p $TERMUX_PREFIX/lib/pkgconfig
+    # 预防性地把 Rutabaga 可能查岗的子模块全给造出来！
+    for libname in aemu_base aemu_logging gfxstream_host_common; do
+        cat << PC_EOF > $TERMUX_PREFIX/lib/pkgconfig/${libname}.pc
+Name: ${libname}
+Description: Fake ${libname} for rutabaga build bypass
+Version: 1.0.0
+Libs: -L${TERMUX_PREFIX}/lib -lgfxstream_backend
+Cflags: -I${TERMUX_PREFIX}/include
+PC_EOF
+    done
+
     cd $TERMUX_PKG_SRCDIR/repo/ffi
     
     echo "[*] 启动 Cargo 编译，三神装全开！"
